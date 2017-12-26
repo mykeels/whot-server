bf6856619593331894e1c0369bb48fcfd81b189b
Ikechi Michael <mykehell123@gmail.com>
2017-12-26 08:45:37 +0100

    chore: removed irrelevant stuff

--- index.js
+++ index.js
@@ -15,10 +15,8 @@ app.use(function(req, res, next) {
 })
 
 app.use(function(err, req, res, next) {
-    res.locals.message = err.message
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
     res.status(err.status || 500)
-    res.render('error')
+    res.send(err)
 })
 
 var listener = app.listen(process.env.PORT || 8800, function(){
