a888d3296ef2398c9fccdf5dd466c57f75efde10
Ikechi Michael <mykehell123@gmail.com>
2017-12-31 13:52:38 +0100

    feat: log error with red text

--- routes/socket.route.js
+++ routes/socket.route.js
@@ -5,14 +5,18 @@ const Game = require('whot')
 const GameFactory = require('../factories/game.factory')
 const logger = require('../logger')('socket.route.js')
 
+const Events = {
+    GAME_CREATE: 'game:create'
+}
+
 const extendWs = require('../prototypes/ws.prototype')
 
 const wsErrorHandler = (err) => {
     if (err) logger.error("ws-error", err)
-  }
+}
 
 const errorThrow = (err, ws) => {
-    console.error(err)
+    logger.error(err)
     if (ws) {
         if (ws.readyState === ws.OPEN) {
             ws.send(JSON.stringify({
@@ -33,20 +37,30 @@ module.exports = (app, factory = new GameFactory()) => {
             const instance = factory.get(id)
             if (instance) {
                 const game = instance.game
-        
-                if (instance.sockets.players.length < game.turn.count() && instance.sockets.players.indexOf(ws) < 0) {
-                    instance.sockets.players.push(ws)
-                }
-                else {
-                    instance.sockets.listeners.push(ws)
+                
+                if (!(ws.id && ws.type)) {
+                    if (instance.sockets.players.length < game.turn.count() && instance.sockets.players.indexOf(ws) < 0) {
+                        ws.id = instance.sockets.players.length + 1
+                        ws.type = 'player'
+                        instance.sockets.players.push(ws)
+                    }
+                    else {
+                        ws.id = instance.sockets.listeners.length + 1
+                        ws.type = 'listener'
+                        instance.sockets.listeners.push(ws)
+                    }
                 }
 
                 //onopen
-                ws.json(Object.assign({ id }, instance.sockets.stats()))
+                ws.json(Object.assign({ id, message: Events.GAME_CREATE, playerId: ws.id, type: ws.type }, instance.sockets.stats()))
         
                 //onmessage
                 ws.on('message', (message = '') => {
-
+                    const messageData = JSON.parse(message)
+                    switch (messageData.message) {
+                        case '':
+                        break;
+                    }
                 })
             }
             else {
